26f087874a7c3bb6b60aab1c96239dc046085c47
Divyesh Vishwakarma <workdivyeshvishwakarma@gmail.com>
2023-07-10 17:32:35 +0530

    Compare My and ISS Location

--- iss/home/views.py
+++ iss/home/views.py
@@ -1,5 +1,19 @@
+import json
 from django.shortcuts import render
-
+import requests
 # Create your views here.
 def index(request):
-    return render(request, 'home/index.html')
+    response = requests.get("http://api.open-notify.org/iss-now.json")
+    data = response.content
+    processedData = json.loads(data)
+    isslat = processedData["iss_position"]["latitude"]
+    isslon = processedData["iss_position"]["longitude"]
+    mylat = '19.08632'
+    mylon = '72.89661'
+    isIssOverMyHead = (isslat == mylat and isslon == mylon)
+    context = {
+        "data": {"isslat":isslat, "isslon": isslon}, 
+        "isIssOverMyHead": isIssOverMyHead,
+        "myData": {"mylat":mylat, "mylon": mylon}
+    }
+    return render(request, 'home/index.html', context)
